aa2c923a414ae42246ee255f3bb1d4ea31d69d30
Diana <dianapride.s@gmail.com>
2025-12-11 17:56:30 +0200

    feat: add NuclearFuelBatch and NuclearMaterialRecord entities

---
+++ AESAccounting/AES.Core/Entities/NuclearFuelBatch.cs
@@ -0,0 +1,14 @@
+﻿namespace AES.Core.Entities;
+
+public class NuclearFuelBatch
+{
+    public int Id { get; set; }
+    public string BatchNumber { get; set; } = null!;           // № ТВЗ
+    public DateTime ReceiptDate { get; set; } = DateTime.UtcNow;
+    public decimal UraniumMassKg { get; set; }
+    public decimal EnrichmentPercent { get; set; }
+    public string StorageZone { get; set; } = null!;
+    public string IsotopeComposition { get; set; } = null!;
+
+    public List<NuclearMaterialRecord> MaterialRecords { get; set; } = new();
+}
---
+++ AESAccounting/AES.Core/Entities/NuclearMaterialRecord.cs
@@ -0,0 +1,12 @@
+﻿namespace AES.Core.Entities;
+
+public class NuclearMaterialRecord
+{
+    public int Id { get; set; }
+    public int BatchId { get; set; }
+    public NuclearFuelBatch Batch { get; set; } = null!;
+
+    public string MaterialNumber { get; set; } = null!;       // например ЯМ-20251211-ТВЗ123
+    public DateTime RegistrationDate { get; set; } = DateTime.UtcNow;
+    public string Status { get; set; } = "Зареєстровано";
+}
